6ae50179719ece18f182188b762a174851f760bd
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-11 16:32:37 +0100

    Docking Station capacity limited

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,12 +3,13 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def dock(bike)
+    fail 'Docking station full' if @bike
+    @bike = bike
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
-  end
-
-  def dock(bike)
-    @bike = Bike.new
   end
 end
